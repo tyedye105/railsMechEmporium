005bc881756b976198d2e3e54b36921a9bd6cdc8
Marty Kovach <tyedye105@yahoo.com>
2017-02-10 11:41:28 -0800

    two passing model specs

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -1,3 +1,4 @@
 class Bear < ActiveRecord::Base
   validates :name, :presence => true
+  validates :cost, :presence => true
 end
--- spec/models/bear_spec.rb
+++ spec/models/bear_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Bear do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
 end
